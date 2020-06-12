cb55afe12c117beeb47ba81d12a982943151fd1c
KingBob <52259912+Kingbobs@users.noreply.github.com>
2020-06-11 22:36:39 -0600

    Update Main.php

--- src/Infernus101/Main.php
+++ src/Infernus101/Main.php
@@ -247,7 +247,7 @@ class Main extends PluginBase implements Listener{
 				   $bounty = $this->getBountyMoney($lower);
 				   $sender->sendMessage("§d=+=+=+=+=+=+= §bBounty §d=+=+=+=+=+=+=\n§aBounty on $args[1]: §6$bounty"."$\n§d=+=+=+=+=+=+= §bBounty §d=+=+=+=+=+=+=");
 				   return true;
-			    }
+				   break;
 			   }
 			       break;
 		   case "top":
@@ -266,15 +266,15 @@ class Main extends PluginBase implements Listener{
 						    $i++; 
 			              }
 		        return true; 
-		 }
+		    break;
 		   case "about":
 		    $sender->sendMessage("§5Bounty v2.0.0-beta1 by §aKingbobs. n§eThis plugin was bought to you by §6Fallen§bMc§c Server! §aOur server IP: §c5.199.143.143 Port - 25579");
 		   return true; 
-		 }   
+		   break;   
 		   default:
 		    $sender->sendMessage("§dPlease use: §5/bounty <set | me | search | top | about>");
 		    return true;
-		 }
+		    break;
 			 }
 	}
   }
